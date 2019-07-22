88d3e84d39f3454cb43cb128a2e30c6a22276236
quanta2015 <liyangtom@163.com>
2019-07-22 20:29:22 +0900

    add reg server

--- src/app/reg/index.js
+++ src/app/reg/index.js
@@ -19,23 +19,16 @@ class Reg extends React.Component {
     super(props)
     this.actions = props.userActions
     this.store   = props.userStore
+
+    this.state = {
+      showexp: false
+    }
   }
 
 
   doReg = (e) =>{
     e.preventDefault();
-    // this.props.form.validateFields({ force: true }, (err, values) => {
-    //   if (!err) {
-    //     console.log('Received values of form: ', values);
-    //     let pwd = values.pwd
-    //     let repwd = values.repwd
-    //     if (pwd !== repwd) {
-    //       message.success('r.msg')
-    //     }
-    //   }
-    // });
-
-
+    
 
     this.props.form.validateFields(async (err, values) => {
       if (!err) {
@@ -53,11 +46,18 @@ class Reg extends React.Component {
     })
   }
 
+  showExp = (e)=>{
+    this.setState({
+      showexp: e
+    })
+  }
+
 
 
   render() {
     const { getFieldDecorator } = this.props.form;
     let tabList = [0,0,0]
+    let { showexp } = this.state
 
     return (
       <div className='g-reg'>
@@ -67,55 +67,54 @@ class Reg extends React.Component {
           <Form className="m-reg-form" labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
             <Form.Item label="メールアドレス">
               {getFieldDecorator('email', {
-                rules: [{ required: true, message: 'Please input your email!' }],
+                rules: [{ required: true, message: 'メールアドレスを入力してください' }],
               })(<Input />)}
             </Form.Item>
             <Form.Item label="パスワード">
               {getFieldDecorator('pwd', {
-                rules: [{ required: true, message: 'Please input your password!' }],
+                rules: [{ required: true, message: 'パスワードを入力してください' }],
               })(<Input.Password placeholder="パスワード"/>)}
             </Form.Item>
             <Form.Item label="パスワード（確認用)">
               {getFieldDecorator('repwd', {
-                rules: [{ required: true, message: 'Please input your password!' }],
+                rules: [{ required: true, message: 'パスワードを再入力してください' }],
               })(<Input.Password placeholder="パスワード"/>)}
             </Form.Item>
             <Form.Item label="氏名">
               {getFieldDecorator('name-cn', {
-                rules: [{ required: true, message: 'Please input your password!' }],
+                rules: [{ required: true, message: '氏名を入力してください' }],
               })(<Input placeholder="自由陣　太郎" />)}
             </Form.Item>
             <Form.Item label="氏名（カナ）">
               {getFieldDecorator('name-kj', {
-                rules: [{ required: true, message: 'Please input your password!' }],
+                rules: [{ required: true, message: '氏名（カナ）を入力してください' }],
               })(<Input.Password placeholder="ジユウジン　タロウ"/>)}
             </Form.Item>
             <Form.Item label="生まれた年">
               {getFieldDecorator('name-kn', {
-                rules: [{ required: true, message: 'Please input your password!' }],
+                rules: [{ required: true, message: '生まれた年を入力してください' }],
               })(<DatePicker />)}
             </Form.Item>
             <Form.Item label="電話番号">
               {getFieldDecorator('phone', {
-                rules: [{ required: true, type:'number', min:10000000000, max:99999999999,  message: 'Please input your password!' }],
+                rules: [{ required: true, type:'number', min:10000000000, max:99999999999,  message: '電話番号を入力してください' }],
               })(<InputNumber placeholder="01234567890（ハイフン不要）" style={{width:'100%'}} />)}
             </Form.Item>
             <Form.Item label="カテゴリー">
               {getFieldDecorator('pers_type', {
                 initialValue: "0",
-                rules: [{ required: true, type: 'string', messge: 'Please input your password!' }]
+                rules: [{ required: true, type: 'string', message: 'カテゴリーを選択してください' }]
                 
               })(<Select className="m-form-text">
-                  <Option value="0">ない</Option>
-                  <Option value="1">フリーランス</Option>
-                  <Option value="2">副業</Option>
+                  <Option value="0">フリーランス</Option>
+                  <Option value="1">副業</Option>
                 </Select>)}
             </Form.Item>
 
             <h2 className="m-reg_h2">稼働希望をご入力ください</h2>
             <Form.Item label="勤務希望エリア">
               {getFieldDecorator('work_area', {
-                rules: [{ required: true, type: 'array', messge: 'Please input your password!' }],
+                rules: [{ required: true, type: 'array', message: '勤務希望エリアを選択してください' }],
                 initialValue: ["0"]
               })(<Select mode="multiple" className="m-form-text">
                   <Option value="0">２３区</Option>
@@ -130,7 +129,7 @@ class Reg extends React.Component {
             </Form.Item>
             <Form.Item label="希望稼働時期">
               {getFieldDecorator('work_time', {
-                rules: [{ required: true, type: 'array', messge: 'Please input your password!' }],
+                rules: [{ required: true, type: 'array', message: '希望稼働時期を選択してください' }],
                 initialValue: ["0"]
               })(<Select mode="multiple" className="m-form-text">
                   <Option value="0">1ヶ月</Option>
@@ -142,13 +141,13 @@ class Reg extends React.Component {
             </Form.Item>
             <Form.Item label="希望月額報酬（万円）">
               {getFieldDecorator('work_mony', {
-                rules: [{ type: 'integer', required: true,  min:1, max:200, messge: 'Please input your password!' }],
+                rules: [{ type: 'integer', required: true,  min:1, max:200, message: '希望月額報酬を入力してください（最大200万円）' }],
                 initialValue: 10
               })(<InputNumber placeholder="数字（単位：万円）" style={{width:'100%'}} />)}
             </Form.Item>
             <Form.Item label="希望働き方">
               {getFieldDecorator('work_type', {
-                rules: [{ required: true, type: 'array', messge: 'Please input your password!' }],
+                rules: [{ required: true, type: 'array', message: '希望働き方を選択してください' }],
                 initialValue: ["0"]
               })(<Select mode="multiple" className="m-form-text">
                   <Option value="0">常駐</Option>
@@ -163,93 +162,100 @@ class Reg extends React.Component {
         
             <h2 className="m-reg_h2">
               <span className="m-h2-ti">経験・スキル登録</span>
-              <Switch defaultChecked />
+              <Switch onChange={this.showExp} />
             </h2>
             
-            <Tabs defaultActiveKey="1">
-            {tabList.map((item,index)=>{
-              return (
-              <TabPane tab={`案件${index+1}`} key={index+1}>
-                <div className="m-row">
-                  <div className="m-ti m-require">案件名</div>
-                  <div className="m-co">
-                    <Input className="m-form-text form-control" placeholder="xxxxx@yahoo.co.jp" required/>
-                    
-                  </div>
-                </div>
-                <div className="m-row">
-                  <div className="m-ti">期間</div>
-                  <div className="m-co">
-                    <RangePicker defaultValue={[moment('2015/01/01', dateFormat), moment('2015/01/01', dateFormat)]}
-                                 format={dateFormat}　
-                                 className="m-form-text"/>
-                  </div>
-                </div>
-                <div className="m-row">
-                  <div className="m-ti">経験言語</div>
-                    <div className="m-co">
-                    <Select mode="multiple" defaultValue={['0']} className="m-form-text">
-                      <Option value="0">Java</Option>
-                      <Option value="1">ASP</Option>
-                      <Option value="2">PHP</Option>
-                      <Option value="3">Perl</Option>
-                      <Option value="4">Struts</Option>
-                      <Option value="5">HTML</Option>
-                      <Option value="6">JavaScript</Option>
-                      <Option value="1">.NET</Option>
-                      <Option value="2">XML</Option>
-                      <Option value="3">VB</Option>
-                      <Option value="4">Script</Option>
-                      <Option value="4">その他</Option>
-                    </Select>
-                  </div>
-                </div>
-                <div className="m-row">
-                  <div className="m-ti">経験職種</div>
-                    <div className="m-co">
-                    <Select mode="multiple" defaultValue={['0']} className="m-form-text">
-                      <Option value="0">システムエンジニア</Option>
-                      <Option value="1">プログラマ</Option>
-                      <Option value="2">Webエンジニア</Option>
-                      <Option value="3">ネットワークエンジニア</Option>
-                      <Option value="4">運用保守</Option>
-                      <Option value="5">データベースエンジニア</Option>
-                      <Option value="6">PM/PL/コンサル</Option>
-                      <Option value="7">評価・テスト</Option>
-                      <Option value="8">ヘルプデスク</Option>
-                      <Option value="9">SE支援その他</Option>
-                    </Select>
-                  </div>
-                </div>
-                <div className="m-row">
-                  <div className="m-ti">経験工程</div>
-                    <div className="m-co">
-                    <Select mode="multiple" defaultValue={['0']} className="m-form-text">
-                      <Option value="0">保険</Option>
-                      <Option value="1">流通</Option>
-                      <Option value="2">金融</Option>
-                      <Option value="3">証券</Option>
-                      <Option value="4">製造</Option>
-                      <Option value="5">運輸</Option>
-                      <Option value="6">通信</Option>
-                      <Option value="7">官公庁</Option>
-                      <Option value="8">教育</Option>
-                      <Option value="9">医療</Option>
-                      <Option value="10">情報</Option>
-                      <Option value="11">その他</Option>
-                    </Select>
-                  </div>
-                </div>
-                <div className="m-row">
-                  <div className="m-ti">詳細</div>
-                    <div className="m-co">
-                    <TextArea rows={4} />
-                  </div>
-                </div>
-              </TabPane>
-              ) 
-            })}
-            </Tabs>
+            {showexp?(
+              <Tabs defaultActiveKey="1">
+                {tabList.map((item,index)=>{
+                  return (
+                  <TabPane tab={`案件${index+1}`} key={index+1}>
+                    <Form.Item label="案件名">
+                    {getFieldDecorator('proj_name', {
+                      rules: [{ required: true, type: 'string', message: '案件名を入力してください' }],
+                      initialValue: "案件名"
+                    })(<Input placeholder="案件名" />)}
+                    </Form.Item>
+
+                    <Form.Item label="期間">
+                    {getFieldDecorator(['data_from','data_to'], {
+                      rules: [{ required: true,  message: '期間を選択してください' }],
+                    })(<RangePicker defaultValue={[moment('2015/01/01', dateFormat), moment('2015/01/01', dateFormat)]}
+                                       format={dateFormat}　
+                                       className="m-form-text"/>)}
+                    </Form.Item>
+
+                    <Form.Item label="経験言語">
+                      {getFieldDecorator('work_lang', {
+                        rules: [{ required: true, type: 'array', message: '経験言語を選択してください' }],
+                        initialValue: ["0"]
+                      })(<Select mode="multiple" defaultValue={['0']} className="m-form-text">
+                          <Option value="0">Java</Option>
+                          <Option value="1">ASP</Option>
+                          <Option value="2">PHP</Option>
+                          <Option value="3">Perl</Option>
+                          <Option value="4">Struts</Option>
+                          <Option value="5">HTML</Option>
+                          <Option value="6">JavaScript</Option>
+                          <Option value="7">.NET</Option>
+                          <Option value="8">XML</Option>
+                          <Option value="9">VB</Option>
+                          <Option value="10">Script</Option>
+                          <Option value="11">その他</Option>
+                        </Select>)}
+                    </Form.Item>
+
+                    <Form.Item label="経験職種">
+                      {getFieldDecorator('work_role', {
+                        rules: [{ required: true, type: 'array', message: '経験職種を選択してください' }],
+                        initialValue: ["0"]
+                      })(<Select mode="multiple" defaultValue={['0']} className="m-form-text">
+                          <Option value="0">システムエンジニア</Option>
+                          <Option value="1">プログラマ</Option>
+                          <Option value="2">Webエンジニア</Option>
+                          <Option value="3">ネットワークエンジニア</Option>
+                          <Option value="4">運用保守</Option>
+                          <Option value="5">データベースエンジニア</Option>
+                          <Option value="6">PM/PL/コンサル</Option>
+                          <Option value="7">評価・テスト</Option>
+                          <Option value="8">ヘルプデスク</Option>
+                          <Option value="9">SE支援その他</Option>
+                        </Select>)}
+                    </Form.Item>
+
+                    <Form.Item label="経験工程">
+                      {getFieldDecorator('work_proj', {
+                        rules: [{ required: true, type: 'array', message: '経験工程を選択してください' }],
+                        initialValue: ["0"]
+                      })(<Select mode="multiple" defaultValue={['0']} className="m-form-text">
+                          <Option value="0">保険</Option>
+                          <Option value="1">流通</Option>
+                          <Option value="2">金融</Option>
+                          <Option value="3">証券</Option>
+                          <Option value="4">製造</Option>
+                          <Option value="5">運輸</Option>
+                          <Option value="6">通信</Option>
+                          <Option value="7">官公庁</Option>
+                          <Option value="8">教育</Option>
+                          <Option value="9">医療</Option>
+                          <Option value="10">情報</Option>
+                          <Option value="11">その他</Option>
+                        </Select>)}
+                    </Form.Item>
+
+                    <Form.Item label="詳細">
+                      {getFieldDecorator('work_detl', {
+                        rules: [{ required: true, type: 'string', message: '詳細を入力してください' }],
+                      })(<TextArea rows={4} />)}
+                    </Form.Item>
+
+                  </TabPane>
+                  ) 
+                })}
+              </Tabs>
+            ):(<div></div>)}
+
+            
 
 
             <div className="m-row fn-frc">
