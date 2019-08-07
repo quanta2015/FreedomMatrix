5a1eb31ee5d377d04c49566d0e9580823763c9ff
quanta2015 <liyangtom@163.com>
2019-08-07 23:56:50 +0900

    修复案件登录

--- src/constant/data.js
+++ src/constant/data.js
@@ -4,7 +4,7 @@ export const MONTH_FORMAT = 'YYYY/MM'
 
 export const MAIN_MENU = [{ title:'案件検索', icon:'search',  path:'/projquery', type: 0 },{
                             title:'案件登録', icon:'addproj', path:'/projadd',   type: 2 },{
-                            title:'会員HOME', icon:'user',    path:'/homeuser',  type: 0 },{
+                            title:'会員HOME', icon:'user',    path:'/homeuser',  type: 1 },{
                             title:'会社HOME', icon:'user',    path:'/homecomp',  type: 2 },{
                             title:'お問い合わせ', icon:'question-circle', path:'/ask', type: 0 },{
                             title: 'その他', icon:'user', type: 0, submenu:  
